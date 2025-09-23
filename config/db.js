d9b5a1fcc799b495a1134fa79a62cc6281c8894a
Ahammed yaseen <yaseenvava76@gmail.com>
2025-09-23 15:01:13 +0530

    googleAuthed

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const connectDB = async ()=>{
       await mongoose.connect(process.env.MONGODB_URI)
       console.log("DB connected");
     }catch(error){
-     console.log('DB connection error'.error.message)
+     console.log('DB connection error',error.message)
      process.exit(1)
     }
 }
